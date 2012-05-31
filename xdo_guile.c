2bc818ec4e20cbb308448ac39c1e8670982efa16
Krister Svanlund <krister.svanlund@gmail.com>
2012-05-31 18:40:11 +0200

    Renamed all functions to start with 'lib:'

--- xdo_guile.c
+++ xdo_guile.c
@@ -585,123 +585,123 @@ void
 init_xdo_libxdo(void *unused)
 {
     setup_smobs();
-    scm_c_define_gsubr("xdo-new", 0, 1, 0, xdo_new_wrapper);
-    scm_c_define_gsubr("xdo-version", 0, 0, 0, xdo_version_wrapper);
-    scm_c_define_gsubr("xdo-move-mouse", 4, 0, 0, xdo_move_mouse_wrapper);
-    scm_c_define_gsubr("xdo-move-mouse-relative-to-window", 4, 0, 0, xdo_move_mouse_rel_to_win_wrapper);
-    scm_c_define_gsubr("xdo-move-mouse-relative", 3, 0, 0, xdo_move_mouse_rel_wrapper);
-    scm_c_define_gsubr("xdo-mouse-down", 3, 0, 0, xdo_mouse_down_wrapper);
-    scm_c_define_gsubr("xdo-mouse-up", 3, 1, 0, xdo_mouse_up_wrapper);
-    scm_c_define_gsubr("xdo-get-mouse-location", 1, 0, 0, xdo_get_mouse_location_wrapper);
-    scm_c_define_gsubr("xdo-wait-for-mouse-move", 3, 1, 0, xdo_wait_for_mouse_move_wrapper);
-    scm_c_define_gsubr("xdo-click-window", 3, 2, 0, xdo_click_window_wrapper);
-    scm_c_define_gsubr("xdo-enter-text-window", 3, 1, 0, xdo_enter_text_window_wrapper);
-    scm_c_define_gsubr("xdo-send-keysequence-window", 3, 2, 0, xdo_send_keysequence_window_wrapper);
-    scm_c_define_gsubr("xdo-move-window", 4, 0, 0, xdo_move_window_wrapper);
-    scm_c_define_gsubr("xdo-translate-window-with-sizehint", 4, 0, 0, xdo_translate_window_with_sizehint);
-    scm_c_define_gsubr("xdo-set-windo-size", 5, 0, 0, xdo_set_window_size_wrapper);
-    scm_c_define_gsubr("xdo-set-window-property", 4, 0, 0, xdo_set_window_property_wrapper);
-    scm_c_define_gsubr("xdo-set-window-class", 4, 0, 0, xdo_set_window_class_wrapper);
-    scm_c_define_gsubr("xdo-set-window-urgency", 3, 0, 0, xdo_set_window_urgency_wrapper);
-    scm_c_define_gsubr("xdo-set-window-override-redirect", 3, 0, 0, xdo_set_window_override_redirect_wrapper);
-    scm_c_define_gsubr("xdo-focus-window", 2, 0, 0, xdo_focus_window_wrapper);
-    scm_c_define_gsubr("xdo-raise-window", 2, 0, 0, xdo_raise_window_wrapper);
-    scm_c_define_gsubr("xdo-get-focused-window", 1, 0, 0, xdo_get_focused_window_wrapper);
-    scm_c_define_gsubr("xdo-wait-for-window-focus", 3, 0, 0, xdo_wait_for_window_focus_wrapper);
-    scm_c_define_gsubr("xdo-get-pid-window", 2, 0, 0, xdo_get_pid_window_wrapper);
-    scm_c_define_gsubr("xdo-get-focused-window-sane", 1, 0, 0, xdo_get_focused_window_sane_wrapper);
-    scm_c_define_gsubr("xdo-activate-window", 2, 0, 0, xdo_activate_window_wrapper);
-    scm_c_define_gsubr("xdo-wait-for-window-active", 3, 0, 0, xdo_wait_for_window_active_wrapper);
-    scm_c_define_gsubr("xdo-map-window", 2, 0, 0, xdo_map_window_wrapper);
-    scm_c_define_gsubr("xdo-unmap-window", 2, 0, 0, xdo_unmap_window_wrapper);
-    scm_c_define_gsubr("xdo-minimize-window", 2, 0, 0, xdo_minimize_window_wrapper);
-    scm_c_define_gsubr("xdo-reparent-window", 3, 0, 0, xdo_reparent_window_wrapper);
-    scm_c_define_gsubr("xdo-get-window-location", 2, 0, 0, xdo_get_window_location_wrapper);
-    scm_c_define_gsubr("xdo-get-window-size", 2, 0, 0, xdo_get_window_size_wrapper);
-    scm_c_define_gsubr("xdo-get-active-window", 1, 0, 0, xdo_get_active_window_wrapper);
-    scm_c_define_gsubr("xdo-select-window-with-click", 1, 0, 0, xdo_select_window_with_click_wrapper);
-    scm_c_define_gsubr("xdo-set-number-of-desktops", 2, 0, 0, xdo_set_number_of_desktops_wrapper);
-    scm_c_define_gsubr("xdo-get-number-of-desktops", 1, 0, 0, xdo_get_number_of_desktops_wrapper);
-    scm_c_define_gsubr("xdo-set-current-desktop", 2, 0, 0, xdo_set_current_desktop_wrapper);
-    scm_c_define_gsubr("xdo-get-current-desktop", 1, 0, 0, xdo_get_current_desktop_wrapper);
-    scm_c_define_gsubr("xdo-set-desktop-for-window", 2, 0, 0, xdo_set_desktop_for_window_wrapper);
-    scm_c_define_gsubr("xdo-get-desktop-for-window", 1, 0, 0, xdo_get_desktop_for_window_wrapper);
-    scm_c_define_gsubr("xdo-search-windows", 2, 0, 0, xdo_search_windows_wrapper);
-    scm_c_define_gsubr("xdo-get-window-property", 3, 0, 0, xdo_get_window_property_wrapper);
-    scm_c_define_gsubr("xdo-get-input-state", 1, 0, 0, xdo_get_input_state_wrapper);
+    scm_c_define_gsubr("lib:xdo-new", 0, 1, 0, xdo_new_wrapper);
+    scm_c_define_gsubr("lib:xdo-version", 0, 0, 0, xdo_version_wrapper);
+    scm_c_define_gsubr("lib:xdo-move-mouse", 4, 0, 0, xdo_move_mouse_wrapper);
+    scm_c_define_gsubr("lib:xdo-move-mouse-relative-to-window", 4, 0, 0, xdo_move_mouse_rel_to_win_wrapper);
+    scm_c_define_gsubr("lib:xdo-move-mouse-relative", 3, 0, 0, xdo_move_mouse_rel_wrapper);
+    scm_c_define_gsubr("lib:xdo-mouse-down", 3, 0, 0, xdo_mouse_down_wrapper);
+    scm_c_define_gsubr("lib:xdo-mouse-up", 3, 1, 0, xdo_mouse_up_wrapper);
+    scm_c_define_gsubr("lib:xdo-get-mouse-location", 1, 0, 0, xdo_get_mouse_location_wrapper);
+    scm_c_define_gsubr("lib:xdo-wait-for-mouse-move", 3, 1, 0, xdo_wait_for_mouse_move_wrapper);
+    scm_c_define_gsubr("lib:xdo-click-window", 3, 2, 0, xdo_click_window_wrapper);
+    scm_c_define_gsubr("lib:xdo-enter-text-window", 3, 1, 0, xdo_enter_text_window_wrapper);
+    scm_c_define_gsubr("lib:xdo-send-keysequence-window", 3, 2, 0, xdo_send_keysequence_window_wrapper);
+    scm_c_define_gsubr("lib:xdo-move-window", 4, 0, 0, xdo_move_window_wrapper);
+    scm_c_define_gsubr("lib:xdo-translate-window-with-sizehint", 4, 0, 0, xdo_translate_window_with_sizehint);
+    scm_c_define_gsubr("lib:xdo-set-windo-size", 5, 0, 0, xdo_set_window_size_wrapper);
+    scm_c_define_gsubr("lib:xdo-set-window-property", 4, 0, 0, xdo_set_window_property_wrapper);
+    scm_c_define_gsubr("lib:xdo-set-window-class", 4, 0, 0, xdo_set_window_class_wrapper);
+    scm_c_define_gsubr("lib:xdo-set-window-urgency", 3, 0, 0, xdo_set_window_urgency_wrapper);
+    scm_c_define_gsubr("lib:xdo-set-window-override-redirect", 3, 0, 0, xdo_set_window_override_redirect_wrapper);
+    scm_c_define_gsubr("lib:xdo-focus-window", 2, 0, 0, xdo_focus_window_wrapper);
+    scm_c_define_gsubr("lib:xdo-raise-window", 2, 0, 0, xdo_raise_window_wrapper);
+    scm_c_define_gsubr("lib:xdo-get-focused-window", 1, 0, 0, xdo_get_focused_window_wrapper);
+    scm_c_define_gsubr("lib:xdo-wait-for-window-focus", 3, 0, 0, xdo_wait_for_window_focus_wrapper);
+    scm_c_define_gsubr("lib:xdo-get-pid-window", 2, 0, 0, xdo_get_pid_window_wrapper);
+    scm_c_define_gsubr("lib:xdo-get-focused-window-sane", 1, 0, 0, xdo_get_focused_window_sane_wrapper);
+    scm_c_define_gsubr("lib:xdo-activate-window", 2, 0, 0, xdo_activate_window_wrapper);
+    scm_c_define_gsubr("lib:xdo-wait-for-window-active", 3, 0, 0, xdo_wait_for_window_active_wrapper);
+    scm_c_define_gsubr("lib:xdo-map-window", 2, 0, 0, xdo_map_window_wrapper);
+    scm_c_define_gsubr("lib:xdo-unmap-window", 2, 0, 0, xdo_unmap_window_wrapper);
+    scm_c_define_gsubr("lib:xdo-minimize-window", 2, 0, 0, xdo_minimize_window_wrapper);
+    scm_c_define_gsubr("lib:xdo-reparent-window", 3, 0, 0, xdo_reparent_window_wrapper);
+    scm_c_define_gsubr("lib:xdo-get-window-location", 2, 0, 0, xdo_get_window_location_wrapper);
+    scm_c_define_gsubr("lib:xdo-get-window-size", 2, 0, 0, xdo_get_window_size_wrapper);
+    scm_c_define_gsubr("lib:xdo-get-active-window", 1, 0, 0, xdo_get_active_window_wrapper);
+    scm_c_define_gsubr("lib:xdo-select-window-with-click", 1, 0, 0, xdo_select_window_with_click_wrapper);
+    scm_c_define_gsubr("lib:xdo-set-number-of-desktops", 2, 0, 0, xdo_set_number_of_desktops_wrapper);
+    scm_c_define_gsubr("lib:xdo-get-number-of-desktops", 1, 0, 0, xdo_get_number_of_desktops_wrapper);
+    scm_c_define_gsubr("lib:xdo-set-current-desktop", 2, 0, 0, xdo_set_current_desktop_wrapper);
+    scm_c_define_gsubr("lib:xdo-get-current-desktop", 1, 0, 0, xdo_get_current_desktop_wrapper);
+    scm_c_define_gsubr("lib:xdo-set-desktop-for-window", 2, 0, 0, xdo_set_desktop_for_window_wrapper);
+    scm_c_define_gsubr("lib:xdo-get-desktop-for-window", 1, 0, 0, xdo_get_desktop_for_window_wrapper);
+    scm_c_define_gsubr("lib:xdo-search-windows", 2, 0, 0, xdo_search_windows_wrapper);
+    scm_c_define_gsubr("lib:xdo-get-window-property", 3, 0, 0, xdo_get_window_property_wrapper);
+    scm_c_define_gsubr("lib:xdo-get-input-state", 1, 0, 0, xdo_get_input_state_wrapper);
     /*scm_c_define_gsubr("xdo-keysym-charmap", 0, 0, 0, xdo_keysym_charmap_wrapper);*/
-    scm_c_define_gsubr("xdo-get-symbol-map", 0, 0, 0, xdo_get_symbol_map_wrapper);
-    scm_c_define_gsubr("xdo-get-active-modifiers", 1, 0, 0, xdo_get_active_modifiers_wrapper);
-    scm_c_define_gsubr("xdo-clear-active-modifiers", 3, 0, 0, xdo_clear_active_modifiers_wrapper);
-    scm_c_define_gsubr("xdo-set-active-modifiers", 0, 0, 0, xdo_set_active_modifiers_wrapper);
-    scm_c_define_gsubr("xdo-get-desktop-viewport", 1, 0, 0, xdo_get_desktop_viewport_wrapper);
-    scm_c_define_gsubr("xdo-set-desktop-viewport", 3, 0, 0, xdo_set_desktop_viewport_wrapper);
-    scm_c_define_gsubr("xdo-kill-window", 2, 0, 0, xdo_kill_window_wrapper);
-    scm_c_define_gsubr("xdo-find-window-client", 3, 0, 0, xdo_find_window_client_wrapper);
-    scm_c_define_gsubr("xdo-get-window-name", 2, 0, 0, xdo_get_window_name_wrapper);
-    scm_c_define_gsubr("xdo-disable-feature", 2, 0, 0, xdo_disable_feature_wrapper);
-    scm_c_define_gsubr("xdo-enable-feature", 2, 0, 0, xdo_enable_feature_wrapper);
-    scm_c_define_gsubr("xdo-has-feature", 2, 0, 0, xdo_has_feature_wrapper);
-    scm_c_define_gsubr("xdo-get-viewport-dimensions", 2, 0, 0, xdo_get_viewport_dimensions_wrapper);
+    scm_c_define_gsubr("lib:xdo-get-symbol-map", 0, 0, 0, xdo_get_symbol_map_wrapper);
+    scm_c_define_gsubr("lib:xdo-get-active-modifiers", 1, 0, 0, xdo_get_active_modifiers_wrapper);
+    scm_c_define_gsubr("lib:xdo-clear-active-modifiers", 3, 0, 0, xdo_clear_active_modifiers_wrapper);
+    scm_c_define_gsubr("lib:xdo-set-active-modifiers", 0, 0, 0, xdo_set_active_modifiers_wrapper);
+    scm_c_define_gsubr("lib:xdo-get-desktop-viewport", 1, 0, 0, xdo_get_desktop_viewport_wrapper);
+    scm_c_define_gsubr("lib:xdo-set-desktop-viewport", 3, 0, 0, xdo_set_desktop_viewport_wrapper);
+    scm_c_define_gsubr("lib:xdo-kill-window", 2, 0, 0, xdo_kill_window_wrapper);
+    scm_c_define_gsubr("lib:xdo-find-window-client", 3, 0, 0, xdo_find_window_client_wrapper);
+    scm_c_define_gsubr("lib:xdo-get-window-name", 2, 0, 0, xdo_get_window_name_wrapper);
+    scm_c_define_gsubr("lib:xdo-disable-feature", 2, 0, 0, xdo_disable_feature_wrapper);
+    scm_c_define_gsubr("lib:xdo-enable-feature", 2, 0, 0, xdo_enable_feature_wrapper);
+    scm_c_define_gsubr("lib:xdo-has-feature", 2, 0, 0, xdo_has_feature_wrapper);
+    scm_c_define_gsubr("lib:xdo-get-viewport-dimensions", 2, 0, 0, xdo_get_viewport_dimensions_wrapper);
     scm_c_export(
-        "xdo-new",
-        "xdo-version",
-        "xdo-move-mouse",
-        "xdo-move-mouse-relative-to-window",
-        "xdo-move-mouse-relative",
-        "xdo-mouse-down",
-        "xdo-mouse-up",
-        "xdo-get-mouse-location",
-        "xdo-wait-for-mouse-move",
-        "xdo-click-window",
-        "xdo-enter-text-window",
-        "xdo-send-keysequence-window",
-        "xdo-move-window",
-        "xdo-translate-window-with-sizehint",
-        "xdo-set-windo-size",
-        "xdo-set-window-property",
-        "xdo-set-window-class",
-        "xdo-set-window-urgency",
-        "xdo-set-window-override-redirect",
-        "xdo-focus-window",
-        "xdo-raise-window",
-        "xdo-get-focused-window",
-        "xdo-wait-for-window-focus",
-        "xdo-get-pid-window",
-        "xdo-get-focused-window-sane",
-        "xdo-activate-window",
-        "xdo-wait-for-window-active",
-        "xdo-map-window",
-        "xdo-unmap-window",
-        "xdo-minimize-window",
-        "xdo-reparent-window",
-        "xdo-get-window-location",
-        "xdo-get-window-size",
-        "xdo-get-active-window",
-        "xdo-select-window-with-click",
-        "xdo-set-number-of-desktops",
-        "xdo-get-number-of-desktops",
-        "xdo-set-current-desktop",
-        "xdo-get-current-desktop",
-        "xdo-set-desktop-for-window",
-        "xdo-get-desktop-for-window",
-        "xdo-search-windows",
-        "xdo-get-window-property",
-        "xdo-get-input-state",
+        "lib:xdo-new",
+        "lib:xdo-version",
+        "lib:xdo-move-mouse",
+        "lib:xdo-move-mouse-relative-to-window",
+        "lib:xdo-move-mouse-relative",
+        "lib:xdo-mouse-down",
+        "lib:xdo-mouse-up",
+        "lib:xdo-get-mouse-location",
+        "lib:xdo-wait-for-mouse-move",
+        "lib:xdo-click-window",
+        "lib:xdo-enter-text-window",
+        "lib:xdo-send-keysequence-window",
+        "lib:xdo-move-window",
+        "lib:xdo-translate-window-with-sizehint",
+        "lib:xdo-set-windo-size",
+        "lib:xdo-set-window-property",
+        "lib:xdo-set-window-class",
+        "lib:xdo-set-window-urgency",
+        "lib:xdo-set-window-override-redirect",
+        "lib:xdo-focus-window",
+        "lib:xdo-raise-window",
+        "lib:xdo-get-focused-window",
+        "lib:xdo-wait-for-window-focus",
+        "lib:xdo-get-pid-window",
+        "lib:xdo-get-focused-window-sane",
+        "lib:xdo-activate-window",
+        "lib:xdo-wait-for-window-active",
+        "lib:xdo-map-window",
+        "lib:xdo-unmap-window",
+        "lib:xdo-minimize-window",
+        "lib:xdo-reparent-window",
+        "lib:xdo-get-window-location",
+        "lib:xdo-get-window-size",
+        "lib:xdo-get-active-window",
+        "lib:xdo-select-window-with-click",
+        "lib:xdo-set-number-of-desktops",
+        "lib:xdo-get-number-of-desktops",
+        "lib:xdo-set-current-desktop",
+        "lib:xdo-get-current-desktop",
+        "lib:xdo-set-desktop-for-window",
+        "lib:xdo-get-desktop-for-window",
+        "lib:xdo-search-windows",
+        "lib:xdo-get-window-property",
+        "lib:xdo-get-input-state",
         /*"xdo-keysym-charmap",*/
-        "xdo-get-symbol-map",
-        "xdo-get-active-modifiers",
-        "xdo-clear-active-modifiers",
-        "xdo-set-active-modifiers",
-        "xdo-get-desktop-viewport",
-        "xdo-set-desktop-viewport",
-        "xdo-kill-window",
-        "xdo-find-window-client",
-        "xdo-get-window-name",
-        "xdo-disable-feature",
-        "xdo-enable-feature",
-        "xdo-has-feature",
-        "xdo-get-viewport-dimensions",
+        "lib:xdo-get-symbol-map",
+        "lib:xdo-get-active-modifiers",
+        "lib:xdo-clear-active-modifiers",
+        "lib:xdo-set-active-modifiers",
+        "lib:xdo-get-desktop-viewport",
+        "lib:xdo-set-desktop-viewport",
+        "lib:xdo-kill-window",
+        "lib:xdo-find-window-client",
+        "lib:xdo-get-window-name",
+        "lib:xdo-disable-feature",
+        "lib:xdo-enable-feature",
+        "lib:xdo-has-feature",
+        "lib:xdo-get-viewport-dimensions",
         EXPORT_SMOB_FUNCTIONS
         NULL);
 }
